fadc2f31538eea6e1361953c0d5397f717f66b0b
HarshitDubey007 <harshitdubey1996@gmail.com>
2022-03-30 10:14:15 +0530

    first

--- e-com/client/src/helper/helper.js
+++
@@ -1,9 +0,0 @@
-function getFormData(form) {
-    let formData = new FormData(form);
-    formData = Object.fromEntries(formData.entries());
-    return formData;
-}
-
-module.exports = {
-  getFormData,
-};
