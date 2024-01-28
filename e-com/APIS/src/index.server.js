e5cf7c326a0a6e32fdc594d7a2fc5a8c6c59555d
HarshitDubey007 <harshitdubey1996@gmail.com>
2024-01-29 00:18:11 +0530

    ADMIN, Customer and api

---
+++ e-com/APIS/src/index.server.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const env = require("dotenv");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+
+//routes
+const authRoutes = require("./routes/auth");
+const adminRoutes = require("./routes/admin/auth");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const cartRoutes = require("./routes/cart");
+const initialDataRoutes = require("./routes/admin/initialData");
+const pageRoutes = require("./routes/admin/page");
+const addressRoutes = require("./routes/address");
+const orderRoutes = require("./routes/order");
+const adminOrderRoute = require("./routes/admin/order.routes");
+
+//environment variable or you can say constants
+env.config();
+
+// mongodb connection
+//mongodb+srv://dbUser:dbUser@cluster0.fqkuj.mongodb.net/ecommerce
+mongoose.connect(`mongodb+srv://dbUser:dbuser@cluster0.fqkuj.mongodb.net/ecommerce?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+  }
+)
+  .then(() => {
+    console.log("Database connected");
+  }).catch((error) => {
+    console.log(error)
+  })
+
+  
+app.use(cors());
+app.use(express.json());
+app.use("/public", express.static(path.join(__dirname, "uploads")));
+app.use("/api", authRoutes);
+app.use("/api", adminRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", cartRoutes);
+app.use("/api", initialDataRoutes);
+app.use("/api", pageRoutes);
+app.use("/api", addressRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", adminOrderRoute);
+
+
+if (process.env.NODE_ENV == "production") {
+  app.use("/", express.static("./client/build"))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, './client/build/index.html'))
+  })
+}
+//onst pat = con
+//console.log(pat,"hello:::")
+// if(process.env.NODE_ENV == "production"){
+//   app.use(express.static(path.join(__dirname,"client","build")));
+//   app.get("/*", (req, res ) => {
+//     res.sendFile(path.resolve(__dirname,'client/build/index.html'))
+//   })
+// }
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+
+// async function updateValues() {
+// const EMP = require("./emp")
+
+//   try {
+    
+//     // const a  = await EMP.find({"project.name": "Project-2"  })
+//     // console.log("AA", a)
+//     EMP.deleteMany({ "project.name": "Project-2" , "project.Hrs": 4} ).then(() => {
+//       console.log("Done")
+//     })
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
